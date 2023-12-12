f65652a7281fffb7bc343cda29a83b29efca3c41
andrerodrigues <andrerodrigues29101992@gmail.com>
2023-12-12 01:15:45 +0000

    feat: add not allowed modal

---
+++ src/components/modal/action-not-allowed.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import { Button, Typography } from '@mui/material';
+
+function ActionNotAllowedModal({ open, onClose }) {
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          bgcolor: 'background.paper',
+          boxShadow: 24,
+          p: 4,
+        }}
+      >
+        <Typography variant="h4" textAlign="center" color="error">
+          Danger Zone Ahead!
+        </Typography>
+        <Typography marginY={3.5} textAlign="center">
+          Hold on tight! Taking this action is like playing Jenga on a rollercoaster! 🎢
+          <br />
+          Brace yourself, because <b>this will wipe out the entire dataset</b>, leaving fellow
+          recruiters with nothing but FOMO about my skills as a Web Developer. Don&apos;t worry,
+          though—this action is just a placeholder for now. You can always imagine the data as a
+          vast, empty canvas 🥲.
+        </Typography>
+        <Box display="flex" justifyContent="flex-end">
+          <Button onClick={onClose} variant="contained">
+            Go back
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
+}
+
+ActionNotAllowedModal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default ActionNotAllowedModal;
--- src/sections/bookings/bookings-table-toolbar.jsx
+++ src/sections/bookings/bookings-table-toolbar.jsx
@@ -8,51 +8,61 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import InputAdornment from '@mui/material/InputAdornment';
 
 import Iconify from 'src/components/iconify';
+import ActionNotAllowedModal from 'src/components/modal/action-not-allowed';
+import { useState } from 'react';
 
 // ----------------------------------------------------------------------
 
 export default function BookingsTableToolbar({ numSelected, filterValue, onFilterBooking }) {
-  return (
-    <Toolbar
-      sx={{
-        height: 96,
-        display: 'flex',
-        justifyContent: 'space-between',
-        p: (theme) => theme.spacing(0, 1, 0, 3),
-        ...(numSelected > 0 && {
-          color: 'primary.main',
-          bgcolor: 'primary.lighter',
-        }),
-      }}
-    >
-      {numSelected > 0 ? (
-        <Typography component="div" variant="subtitle1">
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <OutlinedInput
-          value={filterValue}
-          onChange={onFilterBooking}
-          placeholder="Search booking by ID..."
-          startAdornment={
-            <InputAdornment position="start">
-              <Iconify
-                icon="eva:search-fill"
-                sx={{ color: 'text.disabled', width: 20, height: 20 }}
-              />
-            </InputAdornment>
-          }
-        />
-      )}
+  const [modalOpen, setModalOpen] = useState(false);
 
-      {numSelected > 0 && (
-        <Tooltip title="Delete">
-          <IconButton>
-            <Iconify icon="eva:trash-2-fill" />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <>
+      <Toolbar
+        sx={{
+          height: 96,
+          display: 'flex',
+          justifyContent: 'space-between',
+          p: (theme) => theme.spacing(0, 1, 0, 3),
+          ...(numSelected > 0 && {
+            color: 'primary.main',
+            bgcolor: 'primary.lighter',
+          }),
+        }}
+      >
+        {numSelected > 0 ? (
+          <Typography component="div" variant="subtitle1">
+            {numSelected} selected
+          </Typography>
+        ) : (
+          <OutlinedInput
+            value={filterValue}
+            onChange={onFilterBooking}
+            placeholder="Search booking by ID..."
+            startAdornment={
+              <InputAdornment position="start">
+                <Iconify
+                  icon="eva:search-fill"
+                  sx={{ color: 'text.disabled', width: 20, height: 20 }}
+                />
+              </InputAdornment>
+            }
+          />
+        )}
+        {numSelected > 0 && (
+          <Tooltip title="Delete">
+            <IconButton onClick={() => setModalOpen(true)}>
+              <Iconify icon="eva:trash-2-fill" />
+            </IconButton>
+          </Tooltip>
+        )}
+      </Toolbar>
+      <ActionNotAllowedModal open={modalOpen} onClose={handleCloseModal} />
+    </>
   );
 }
 
