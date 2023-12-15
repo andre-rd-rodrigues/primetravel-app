9d22f6f9c84a4c60196369dc8f0082a32308a9a0
andrerodrigues <andrerodrigues29101992@gmail.com>
2023-12-15 14:28:11 +0000

    feat: add firebase analytics and hosting

--- src/app.jsx
+++ src/app.jsx
@@ -7,10 +7,15 @@ import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
 
 import Router from 'src/routes/sections';
 import ThemeProvider from 'src/theme';
+import { useEffect } from 'react';
+import { logEvent } from 'firebase/analytics';
+import { analytics } from './config/firebaseConfig';
 
 // ----------------------------------------------------------------------
 
 export default function App() {
+  useEffect(() => logEvent(analytics, 'app_open'), []);
+
   useScrollToTop();
 
   return (
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,6 +2,7 @@ import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 import { getDatabase } from 'firebase/database';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -19,5 +20,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
 
 export default app;
